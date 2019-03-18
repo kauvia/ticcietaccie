720a361aee32caf027c2be30ca588fec5b830763
kauvia <mkhw1989@gmail.com>
2019-03-18 15:45:08 +0800

    adding AI

--- helpers.js
+++ helpers.js
@@ -1,9 +1,19 @@
-//      HELPER FUNCTIONS AND CONSTS     //
+//      HELPER FUNCTIONS AND VARIABLES     //
 const ranN = (num) => Math.floor(Math.random() * num); //return random number from 0-num
 
 
 
 //      Container for the game              //
 const gameContainer = document.getElementById('game-container');
-//      Game Size for number of grids       //
+const msgContainer = document.getElementById('msg-container');
+
+//      Variables for number of grids       //
 const gameSize = 3;
+let gameGrid = [];
+
+
+//      Boolean to check player turn        //
+let playerTurn = true;
+
+//      AI Difficulty
+let aiMode = "hard";
--- script.js
+++ script.js
@@ -1,42 +1,94 @@
-let gameGrid = [];
-let playerTurn = true;
-
 // Grid class for displaying in browser as well as turn logic
 class Grid {
-	constructor(x, y) {
+	constructor(x, y, aiBaseScore = 0) {
+		//x,y coordinates for displaying in browser
 		this.x = x;
 		this.y = y;
-		this.p1 = false;
-		this.p2 = false;
 		this.display = document.createElement("div");
-		this.updateGrid = this.updateGrid.bind(this);
+
+		// boolean to check if grid already picked
+		this.player = false;
+		this.ai = false;
+
+		//score for AI eval
+		this.aiScore = 0;
+		this.aiBaseScore = aiBaseScore;
 	}
-	displayGrid() {
+	displayGrid = () => {
 		this.display.className = "grid";
 		this.display.style.left = `${this.x}px`;
 		this.display.style.top = `${this.y}px`;
 		gameContainer.appendChild(this.display);
-	}
-	getUserInput() {
-		this.display.addEventListener("click", this.updateGrid);
-	}
-	updateGrid() {
-		if (playerTurn && !this.p1 && !this.p2) {
-			this.p1 = true;
+	};
+	getUserInput = () => {
+		this.display.addEventListener("click", this.updatePlayer);
+	};
+
+	updatePlayer = () => {
+		if (playerTurn && !this.player && !this.ai) {
+			this.player = true;
 			this.display.style.backgroundColor = "red";
 			playerTurn = false;
+			msgContainer.innerHTML = "AI turn.";
+
+			updateAI(gameSize);
 		}
-	}
+	};
 }
 
+const updateAI = x => {
+	for (let i = 0; i < x; i++) {
+		for (let j = 0; j < x; j++) {
+			const grid = gameGrid[i][j];
+
+			// eval rows
+
+
+            let count = 0;
+			//         console.log(grid)
+			for (let x = 0; x < 3; x++) {
+				let targetRowGrid = gameGrid[i][x];
+				let targetColGrid = gameGrid[x][j];
+
+				if (grid !== targetRowGrid && grid != targetColGrid) {
+                    if (targetRowGrid.player){
+                        count++;
+                        
+                    }
+                }
+			}
+            console.log(count)
+			//eval cols
+
+			//eval diags
+		}
+	}
+};
+
 const initializeGame = x => {
-	for (i = 0; i < x; i++) {
+	for (let i = 0; i < x; i++) {
 		gameGrid[i] = [];
-		for (j = 0; j < x; j++) {
+		for (let j = 0; j < x; j++) {
 			gameGrid[i].push(new Grid(j + 100 * j, i + 100 * i));
 			gameGrid[i][j].displayGrid();
 			gameGrid[i][j].getUserInput();
+
+			if (aiMode === "hard") {
+				if ((i === 0 || i === 2) && (j === 0 || j === 2)) {
+					gameGrid[i][j].display.innerHTML = "+1";
+					gameGrid[i][j].aiBaseScore = 1;
+				}
+				if (i === 1 && j === 1) {
+					gameGrid[i][j].display.innerHTML = "+2";
+					gameGrid[i][j].aiBaseScore = 2;
+				}
+			}
 		}
+	}
+	if (playerTurn) {
+		msgContainer.innerHTML = "Your turn.";
+	} else {
+		msgContainer.innerHTML = "AI turn.";
 	}
 };
 
