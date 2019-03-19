05de3fa25bfdf22c2228871e0f5358b858176799
kauvia <mkhw1989@gmail.com>
2019-03-19 21:03:22 +0800

    gonna make some drastic changes

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ class Grid {
 		this.ai = false;
 
 		//score for AI eval
+		this.playerScore = 0;
 		this.aiScore = 0;
 		this.aiBaseScore = aiBaseScore;
 	}
@@ -41,50 +42,99 @@ const updateAI = x => {
 		for (let j = 0; j < 3; j++) {
 			const grid = gameGrid[i][j];
 
-			// eval rows and col
+			//Evaluate Win
 
-			let countRow = 0;
-			let countCol = 0;
+			// Evaluate Block
 
-			for (let x = 0; x < 3; x++) {
-				let targetRowGrid = gameGrid[i][x];
-				let targetColGrid = gameGrid[x][j];
 
-				if (grid !== targetRowGrid) {
-					if (targetRowGrid.player) {
-						countRow++;
-					}
-				}
+			let countRow = evaluateLine("row", i);
+			let countCol = evaluateLine("col", j);
+			let countDiag = [null, null];
 
-				if (grid !== targetColGrid) {
-					if (targetColGrid.player) {
-						countCol++;
-					}
-				}
+			if (
+				((i === 0 || i === 2) && (j === 0 || j === 2)) ||
+				(i === 1 && j === 1)
+			) {
+				countDiag =
+					i === j ? evaluateLine("forwardDiag") : evaluateLine("reverseDiag");
 			}
-			//		console.log(countRow, countCol);
+
+			//Evaluate Block Fork
+
+			//Evaluate Fork
 
 			grid.aiScore = 0;
-			countRow > 1
-				? (grid.aiScore += 100 + grid.aiBaseScore)
-				: countRow > 0
-				? (grid.aiScore += 1 + grid.aiBaseScore)
-				: (grid.aiScore += grid.aiBaseScore);
+			grid.playerScore = 0;
 
-			countCol > 1
-				? (grid.aiScore += 100 + grid.aiBaseScore)
-				: countCol > 0
-				? (grid.aiScore += 1 + grid.aiBaseScore)
-				: (grid.aiScore += grid.aiBaseScore);
-			//eval cols
-
-			//eval diags
+			grid.aiScore =
+				countRow[0] + countCol[0] + countDiag[0] + grid.aiBaseScore;
+			grid.playerScore =
+				countRow[1] + countCol[1] + countDiag[1] + grid.aiBaseScore;
 
 			grid.display.innerHTML = grid.aiScore;
-			console.log(grid.aiScore);
+			grid.display.innerHTML += "<br>";
+
+			grid.display.innerHTML += grid.playerScore;
+			grid.display.innerHTML += "<br>";
 		}
 	}
 	aiMakeMove();
+};
+
+const evaluateLine = (direction, i) => {
+	let playerScore = 0;
+	let aiScore = false;
+
+	let playerPresent = false;
+	let aiPresent = false;
+
+	let targetGrid;
+	for (let x = 0; x < 3; x++) {
+		if (direction === "row") {
+			targetGrid = gameGrid[i][x];
+		} else if (direction === "col") {
+			targetGrid = gameGrid[x][i];
+		} else if (direction === "forwardDiag") {
+			targetGrid = gameGrid[x][x];
+		} else if (direction === "reverseDiag") {
+			targetGrid = gameGrid[2 - x][x];
+		}
+
+		if (targetGrid.player) {
+			playerScore++;
+			playerPresent = true;
+		}
+		if (targetGrid.ai) {
+			aiScore++;
+			aiPresent = true;
+		}
+	}
+
+	if (playerPresent) {
+		aiScore = 0;
+	} else if (aiScore === 3) {
+		aiScore = 1000;
+	} else if (aiScore === 2) {
+		aiScore = 100;
+	} else if (aiScore === 1) {
+		aiScore = 1;
+	} else {
+		aiScore = 0;
+	}
+
+	if (aiPresent) {
+		playerScore = 0;
+	} else if (playerScore === 3) {
+		playerScore = 1000;
+	} else if (playerScore === 2) {
+		playerScore = 100;
+	} else if (playerScore === 1) {
+		playerScore = 1;
+	} else {
+		playerScore = 0;
+	}
+
+	return [aiScore, playerScore];
 };
 
 const aiMakeMove = () => {
@@ -98,7 +148,7 @@ const aiMakeMove = () => {
 			}
 			if (
 				highestScorer &&
-				highestScorer.aiScore < grid.aiScore &&
+				highestScorer.playerScore < grid.playerScore &&
 				!grid.player &&
 				!grid.ai
 			) {
@@ -106,7 +156,7 @@ const aiMakeMove = () => {
 			}
 		}
 	}
-	highestScorer.player = true;
+	highestScorer.ai = true;
 	highestScorer.display.style.backgroundColor = "blue";
 	playerTurn = true;
 	msgContainer.innerHTML = "Your turn.";
@@ -124,12 +174,12 @@ const initializeGame = x => {
 
 			if (aiMode === "hard") {
 				if ((i === 0 || i === 2) && (j === 0 || j === 2)) {
-					gameGrid[i][j].display.innerHTML = "+1";
-					gameGrid[i][j].aiBaseScore = 1;
+					gameGrid[i][j].display.innerHTML = "+0";
+					gameGrid[i][j].aiBaseScore = 0;
 				}
 				if (i === 1 && j === 1) {
-					gameGrid[i][j].display.innerHTML = "+2";
-					gameGrid[i][j].aiBaseScore = 2;
+					gameGrid[i][j].display.innerHTML = "+3";
+					gameGrid[i][j].aiBaseScore = 3;
 				}
 			}
 		}
